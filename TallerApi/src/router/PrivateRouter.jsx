8f2ee667e962071c717d73db6020a16d364619e2
andresrojassshere <82662905+andresrojassshere@users.noreply.github.com>
2023-11-19 23:42:36 -0500

    Delete TallerApi directory

--- TallerApi/src/router/PrivateRouter.jsx
+++
@@ -1,7 +0,0 @@
-import {  Navigate, useLocation} from "react-router-dom";
-
-export function PrivateRoute({children}){
-    const { state } = useLocation();
-    return state?.logged ? children : <Navigate to="/login"/>
-
-}
